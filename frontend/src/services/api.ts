95f0ef61e9651e8b055579ab1020af9eac66f096
DarshanKumarGP <darshankumargp18@gmail.com>
2025-10-19 14:17:12 +0530

    Production build: set env variables and built for deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000/api';
 
 // Create axios instance
 export const api = axios.create({
